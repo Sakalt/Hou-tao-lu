3da60919aa46b3585079c3d55e93b9944e5fc271
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-07-19 07:16:15 +0900

    Update and rename dict.json to dict.js

---
+++ dict.js
@@ -0,0 +1,24 @@
+const dictionary = [
+    {
+        "word": "人",
+        "mean": [
+            {
+                "type": "名詞",
+                "explanation": [
+                    {
+                        "translate": "人。人間。",
+                        "tag": [],
+                        "example": [
+                            {
+                                "ht": "十七億人活在之丸。",
+                                "ja": "この星には81億の人が住む。",
+                                "htPron": "va2 tao3 ya2 tang3 ren2 i1 ao5 bao2。"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+    // ここに他の単語データを追加
+];
