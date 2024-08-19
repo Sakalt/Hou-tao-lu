3f93f8075b19808c1a1fe2ba7a666233511f973f
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-08-19 12:53:54 +0900

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -1,8 +1,8 @@
 const dictionary = [
     {
         word: "人",
-        pronunciation: "luang",
-        bopomofo: "ㄌㄨㄤ",
+        pronunciation: "ren",
+        bopomofo: "ㄖㄣ",
         mean: [
             {
                 type: "noun",
@@ -10,7 +10,7 @@ const dictionary = [
                     {
                         translate: "人間、ひと",
                         example: [
-                            { ht: "人", ja: "ひと", htPron: "luang" }
+                            { ht: "人", ja: "ひと", htPron: "ren" }
                         ]
                     }
                 ]
@@ -19,136 +19,41 @@ const dictionary = [
         relatedWords: ["仁", "衆"]
     },
     {
-        word: "仁",
-        pronunciation: "xa",
-        bopomofo: "ㄒㄚ",
-        mean: [
-            {
-                type: "noun",
-                explanation: [
-                    {
-                        translate: "二人称、あなた",
-                        example: [
-                            { ht: "仁", ja: "あなた", htPron: "xa" }
-                        ]
-                    }
-                ]
-            }
-        ],
-        relatedWords: ["人"]
-    },
-    {
-        word: "従",
-        pronunciation: "ua",
-        bopomofo: "ㄨㄚ",
-        mean: [
-            {
-                type: "verb",
-                explanation: [
-                    {
-                        translate: "従う、いいたりになる",
-                        example: [
-                            { ht: "我仁従", ja: "私はあなたにしたがう", htPron: "xe xa ua" }
-                        ]
-                    }
-                ]
-            }
-        ],
-        relatedWords: ["持力", "力"]
-    },
-    {
-        word: "衆",
-        pronunciation: "kao",
-        bopomofo: "ㄎㄠ",
-        mean: [
-            {
-                type: "noun",
-                explanation: [
-                    {
-                        translate: "三人称、多くの人々",
-                        example: [
-                            { ht: "衆", ja: "おおぜい", htPron: "kao" }
-                        ]
-                    }
-                ]
-            }
-        ],
-        relatedWords: ["人民"]
-    },
-    {
-        word: "徒",
-        pronunciation: "ang",
-        bopomofo: "ㄤ",
-        mean: [
-            {
-                type: "noun",
-                explanation: [
-                    {
-                        translate: "すこし神聖な言い方、えらい人",
-                        example: [
-                            { ht: "徒人", ja: "神聖な人", htPron: "ang luang" }
-                        ]
-                    }
-                ]
-            }
-        ],
-        relatedWords: ["仁", "人"]
-    },
-    {
-        word: "人民",
-        pronunciation: "kao",
-        bopomofo: "ㄎㄠ",
-        mean: [
-            {
-                type: "noun",
-                explanation: [
-                    {
-                        translate: "国民",
-                        example: [
-                            { ht: "人民", ja: "こくみん", htPron: "kao" }
-                        ]
-                    }
-                ]
-            }
-        ],
-        relatedWords: ["衆"]
-    },
-    {
         word: "力",
-        pronunciation: "ao",
-        bopomofo: "ㄠ",
+        pronunciation: "li",
+        bopomofo: "ㄌㄧˋ",
         mean: [
             {
                 type: "noun",
                 explanation: [
                     {
-                        translate: "力,パワー,エネルギー",
+                        translate: "力、パワー、エネルギー",
                         example: [
-                            { ht: "持力", ja: "権力", htPron: "gaon ao" }
+                            { ht: "力", ja: "力", htPron: "li" }
                         ]
                     }
                 ]
             }
         ],
-        relatedWords: ["持力", "従"]
+        relatedWords: ["動力", "機"]
     },
     {
-        word: "持力",
-        pronunciation: "gaon ao",
-        bopomofo: "ㄍㄠㄋ　ㄠ ",
+        word: "可",
+        pronunciation: "kwai",
+        bopomofo: "ㄎㄨㄞˋ",
         mean: [
             {
                 type: "noun",
                 explanation: [
                     {
-                        translate: "権力,腕力,腕前",
+                        translate: "可、可能な",
                         example: [
-                            { ht: "仁持力", ja: "あなたの権力", htPron: "xa gaon ao" }
+                            { ht: "赦可", ja: "許可する", htPron: "xikwai" }
                         ]
                     }
                 ]
             }
         ],
-        relatedWords: ["力", "従"]
+        relatedWords: ["赦", "有"]
     }
 ];
